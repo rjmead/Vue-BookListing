5d47540703862b521093518d1eebfb254a469575
Robert Mead <rmead@gongos.com>
2018-07-16 17:12:40 -0400

    part 3

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>{{book.title}}:{{book.autor}}</li>
+</template>
+
+<script>
+export default {
+    name: "BookItem",
+    props: ['book']
+}
+</script>
+
+<style>
+li{
+        display: block;
+        margin: 0 10px;
+    }
+</style>
